6c1f819e3d8058508b94f6a64c41e3abdc146daf
pleaseplayetoh <m-11067662@moe-dl.edu.my>
2026-05-16 09:01:54 +0800

    sotired

---
+++ app.js
@@ -0,0 +1,22 @@
+//Hi zyu I copied this sry
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', (event) => {
+  event.preventDefault();
+});
