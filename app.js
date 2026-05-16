cc7250af4b455b8ce956230009980eb90cdc2bd6
pleaseplayetoh <m-11067662@moe-dl.edu.my>
2026-05-16 21:05:24 +0800

    bookingupdate1.5

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function myFunction() {
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("dropdown-contents");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
